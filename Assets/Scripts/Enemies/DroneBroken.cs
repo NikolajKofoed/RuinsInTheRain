a4b5e15d66a7868a759698f5373b66ce85041da3
Lucas Valls-Sjøstrand <luv001@edu.zealand.dk>
2025-04-07 15:18:56 +0200

    Drone Enemies
    I have added the two drones variants scripts and they work correctly. But their death does not work because they lack a death animation

---
+++ Assets/Scripts/Enemies/DroneBroken.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class DroneBroken : MonoBehaviour
+{
+    // Movement
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Vector2 moveDirection = new Vector2(1f, 0.5f);
+    [SerializeField] private LayerMask SurfaceLayer;
+    [SerializeField] bool goingUp = true;
+
+    private Rigidbody2D rb;
+	private BoxCollider2D boxCollider;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HitLogic();
+    }
+
+	void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + (moveDirection * Time.deltaTime));
+	}
+
+    // What should happen when the drone hit something
+    private void HitLogic()
+    {
+        if (TouchedGround() && !goingUp)
+        {
+            ChangeYDirection();
+        }
+        if (TouchedRoof() && goingUp)
+        {
+            ChangeYDirection();
+        }
+        if (TouchedWall())
+        {
+            Flip();
+        }
+
+    }
+
+    // Detect if the drone hit a surface and from where
+	private bool TouchedGround()
+	{
+		RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, SurfaceLayer);
+		return raycastHit.collider != null;
+	}
+
+	private bool TouchedRoof()
+	{
+		RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.up, 0.1f, SurfaceLayer);
+		return raycastHit.collider != null;
+	}
+
+	private bool TouchedWall()
+	{
+		RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, SurfaceLayer);
+		return raycastHit.collider != null;
+	}
+
+    // Changes the drones direction
+    private void ChangeYDirection()
+    {
+        moveDirection.y = -moveDirection.y;
+        goingUp = !goingUp;
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(new Vector2(0, 180));
+        moveDirection.x = -moveDirection.x;
+    }
+}
---
+++ Assets/Scripts/Enemies/DroneHunter.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class DroneHunter : MonoBehaviour
+{
+	// Movement
+	[SerializeField] private float speed;
+	[SerializeField] private float lineOfSight;
+	private bool hunterMode = false;
+
+    private Rigidbody2D rb;
+    private Transform player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		float distantanceFromPlayer = Vector2.Distance(player.position, transform.position);
+		if (distantanceFromPlayer < lineOfSight)
+		{
+			hunterMode = true;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if (hunterMode)
+		{
+			transform.position = Vector2.MoveTowards(rb.position, player.position, (speed * Time.deltaTime));
+		}
+		}
+
+    // So we can see what the enemy can see
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSight);
+	}
+}
